dbc281187062f89ee74929d75793ac20b41752dd
PotatoBandana <andy10341@gmail.com>
2024-06-19 05:40:27 +0800

    Ana v1.6

--- Codes/Depart.js
+++ Codes/Depart.js
@@ -6,7 +6,7 @@ window.onload = function(){
     const totalItems = $('.dayWrapper li').length; //按鈕數量
     const visibleItems = 7; //顯示按鈕數量
     const gap = 8; //按鈕之間距離
-    const scrollAmount = (itemWidth + gap) * visibleItems;
+    const scrollAmount = (itemWidth + gap) * visibleItems; //滑動距離
     
     const middlePos = ((totalItems * (itemWidth + gap)) - gap - $('.dayWrapperContainer').width()) / 2; // 計算中間位置
     $('.dayWrapper').scrollLeft(middlePos); // 將中間位置設定成預設
@@ -17,13 +17,13 @@ window.onload = function(){
         $(this).addClass('selected'); 
     });
 
-    $('.dayLeft').click(function(){
+    $('.dayLeft').click(function(){ //選天左滑
         $('.dayWrapper').animate({
             scrollLeft: '-=' + scrollAmount
         }, 400,);
     });
 
-    $('.dayRight').click(function(){
+    $('.dayRight').click(function(){ //選天右滑
         $('.dayWrapper').animate({
             scrollLeft: '+=' + scrollAmount
         }, 400,);
@@ -31,11 +31,8 @@ window.onload = function(){
 
 
     // 頁面寬度偵測------------------------------------
-    var VW;
-    var shift1 = 0;
-    var shitt2 = 0;
-    var shitf3 = 0;
-    var count = 0;    
+    var VW; //紀錄視窗寬度
+    var count = 0; //紀錄點擊次數
     function updateVW() {
         VW = $(window).width();
         // console.log(VW);
@@ -49,10 +46,10 @@ window.onload = function(){
 
     //艙等左右滑動選擇----------------------------------
 
-    $('.scrollRight').click(function(){
+    $('.scrollRight').click(function(){ //向右滑
         var N = $(this).attr('id').substr(1, 1);
-        if(VW >1145){
-            if (count <= 1){
+        if(VW >1145){ //RWD寬螢幕判斷式
+            if (count <= 1){ //如果不是最後一張
                 count++;
                 var po = $('#t' + N + 'P').position().left;
                 shift = shift - po
@@ -60,7 +57,7 @@ window.onload = function(){
                 $('.classSel').stop(true, false).animate({'left': shift}, 500);
                 // console.log(po);
             }
-        }else{
+        }else{ 
             var N = $(this).attr('id').substr(1, 1);
             if (count <= 2){
                 count++;
@@ -73,7 +70,7 @@ window.onload = function(){
         }            
     })
 
-    $('.scrollLeft').click(function(){
+    $('.scrollLeft').click(function(){ //向左滑
         var N = $(this).attr('id').substr(1, 1);
         if(VW >1145){
             if (count > 0){
@@ -97,7 +94,7 @@ window.onload = function(){
         }    
     })
 
-    $('#tab2').click(function(){
+    $('#tab2').click(function(){ //手機版頁簽切換
         if (count <= 0){
             count++;
             var po = $('#t1P').position().left;
@@ -105,12 +102,12 @@ window.onload = function(){
 
             $('.classSel').stop(true, false).animate({'left': shift}, 500);
             $(this).css({'background-color': '#DDE2F5', 'color': '#012064'});
-            $('#tab1').css({'background-color': 'white'});
+            $('#tab1').css({'background-color': 'white'}); //頁簽樣式切換
         }
         
     })
 
-    $('#tab1').click(function(){
+    $('#tab1').click(function(){ //手機版頁簽切換
         if (count > 0){
             count--;
             var po = $('#t1P').position().left;
@@ -127,10 +124,10 @@ window.onload = function(){
 
 //點擊艙等展開清單------------------------------------------
     
-    $('.classSel > li').click(function(){
+    $('.classSel > li').click(function(){ 
         var target = '#' + $(this).attr('id').substr(0, 3) + 'List';
-        $('.levelWrapper').not(target).slideUp();
-        setTimeout(function(){
+        $('.levelWrapper').not(target).slideUp(); //非目標 關閉
+        setTimeout(function(){ //目標開啟
             $(target).slideToggle(400, function() {});
         }, 400);
         console.log(target);
@@ -154,19 +151,22 @@ window.onload = function(){
         e.stopPropagation()
     })
 
+// 去程細節popup--------------------------------------
+    $('#departCard').click(function(){
+        $('#departDetail').css('display', 'block');
+        
+    })
+
+    $('#departDetail').click(function(){
+        $(this).css('display', 'none')
+    })
+
+    $('.fa-circle-xmark').click(function(){
+        $(this).closest('#departDetail').css('display', 'none');
+    });
+
+    $('.fullDetailWrapper').click(function(e){
+        e.stopPropagation()
+    })
 }
 
-
-
-    
-    
-    
-    
-    
-    
-    
-    // document.getElementById('t1Econ').onclick = openList;
-
-    // function openList(){
-    //     getElementById('t1EList').css('display', block)
-    // }
